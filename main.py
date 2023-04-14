eddf64d1d527024698f56900e86a802fbdf38d7c
mThaT1808 <alex_9_2_1@bk.ru>
2023-04-14 20:14:50 +0400

    First commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from FirstTask import FirstTask
+from SecondTask import SecondTask
+from ThirdTask import ThirdTask
+from FourthTask import FourthTask
+
+
+FirstTask()
+
+SecondTask()
+
+ThirdTask()
+
+FourthTask()
+
